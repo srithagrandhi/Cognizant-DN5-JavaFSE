9aeb09c5a6eebd2ebaa1aa6ebe7cc2edfd96d472
srithagrandhi <grandhisritha@gmail.com>
2026-06-17 22:32:37 +0530

    Add files via upload

---
+++ Week1-DesignPatterns/Singleton.java
@@ -0,0 +1,19 @@
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+        System.out.println("Singleton object created!");
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public void showMessage() {
+        System.out.println("Hello From Singleton Pattern");
+    
+    }
+}
